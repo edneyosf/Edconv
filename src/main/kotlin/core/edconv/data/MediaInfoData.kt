b4e8196414be126d67900d60533129f5791cd1ba
Edney Osf <edney.osf@gmail.com>
2025-03-18 09:23:27 -0300

    Media size to Long

--- src/main/kotlin/core/edconv/data/MediaInfoData.kt
+++ src/main/kotlin/core/edconv/data/MediaInfoData.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 
 @Serializable
-data class MediaInfoData(val duration: String, val size: Int) {
+data class MediaInfoData(val duration: String, val size: Long) {
     companion object {
         fun fromJsonString(json: String): MediaInfoData = Json.decodeFromString(json)
     }
