bcc74ebd54a1501ffb9cc6450c5d50bd5a3c0a6d
Edney Osf <edney.osf@gmail.com>
2025-03-18 18:24:56 -0300

    fromString for MediaFormat.kt

--- src/main/kotlin/core/edconv/common/MediaFormat.kt
+++ src/main/kotlin/core/edconv/common/MediaFormat.kt
@@ -1,8 +1,23 @@
 package core.edconv.common
 
+private const val AAC_ = "aac"
+private const val EAC3_ = "eac3"
+private const val H265_ = "h265"
+private const val AV1_ = "av1"
+
 enum class MediaFormat(val codec: String) {
-    AV1("av1"),
-    H265("h265"),
-    EAC3("eac3"),
-    AAC("aac")
+    AAC(AAC_),
+    EAC3(EAC3_),
+    H265(H265_),
+    AV1(AV1_);
+
+    companion object {
+        fun fromString(it: String) = when(it.lowercase()) {
+            AAC_ -> AAC
+            EAC3_ -> EAC3
+            H265_ -> H265
+            AV1_ -> AV1
+            else -> throw Exception("Format not supported")
+        }
+    }
 }
