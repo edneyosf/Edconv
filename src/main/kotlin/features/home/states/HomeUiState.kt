fa20379121e345fa283b385014b2bd46fd98d72e
Edney Osf <edney.osf@gmail.com>
2025-03-18 11:55:51 -0300

    Allowing Error status with empty message

--- src/main/kotlin/features/home/states/HomeUiState.kt
+++ src/main/kotlin/features/home/states/HomeUiState.kt
@@ -1,9 +1,9 @@
-package features.states
+package features.home.states
 
 sealed interface HomeUiState {
     data object Initial: HomeUiState
     data object Loading: HomeUiState
     data class  Progress(val percentage: Float): HomeUiState
     data object Complete: HomeUiState
-    data class Error(val message: String): HomeUiState
+    data class Error(val message: String = ""): HomeUiState
 }
