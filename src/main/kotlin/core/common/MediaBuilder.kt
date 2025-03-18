6598be3c280be0c368eb7c2515ef259948c1f023
Edney Osf <edney.osf@gmail.com>
2025-03-18 08:02:25 -0300

    Adding MediaBuilder.kt

---
+++ src/main/kotlin/core/common/MediaBuilder.kt
@@ -0,0 +1,20 @@
+package core.common
+
+import core.edconv.EdconvArgs
+
+abstract class MediaBuilder(
+    protected open val inputFile: String,
+    protected open val outputFile: String
+) {
+    protected val cmd = mutableListOf<String>()
+
+    fun build(): List<String> {
+        cmd.add(EdconvArgs.INPUT)
+        cmd.add(inputFile)
+
+        cmd.add(EdconvArgs.OUTPUT)
+        cmd.add(outputFile)
+
+        return cmd
+    }
+}
