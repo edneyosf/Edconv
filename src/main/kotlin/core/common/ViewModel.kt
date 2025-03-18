22a1a8081c15f5d18cdb78147a8a9d448daac5f5
Edney Osf <edney.osf@gmail.com>
2025-03-18 14:39:36 -0300

    Renaming ViewModel to Manager

--- src/main/kotlin/core/common/ViewModel.kt
+++
@@ -1,5 +0,0 @@
-package core.common
-
-import kotlinx.coroutines.CoroutineScope
-
-abstract class ViewModel(protected open val scope: CoroutineScope)
--- src/main/kotlin/features/home/states/HomeUiState.kt
+++
@@ -1,9 +0,0 @@
-package features.home.states
-
-sealed interface HomeUiState {
-    data object Initial: HomeUiState
-    data object Loading: HomeUiState
-    data class  Progress(val percentage: Float): HomeUiState
-    data object Complete: HomeUiState
-    data class Error(val message: String = ""): HomeUiState
-}
