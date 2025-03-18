2e17ab6fbb12245918a86acc7922a719d8a5b15f
Edney Osf <edney.osf@gmail.com>
2025-03-18 07:39:41 -0300

    Adding AV1 and H.265 presets

---
+++ src/main/kotlin/core/edconv/args/H265Preset.kt
@@ -0,0 +1,13 @@
+package core.edconv.args
+
+object H265Preset {
+    const val ULTRAFAST = "ultrafast"
+    const val SUPERFAST = "superfast"
+    const val VERYFAST = "veryfast"
+    const val FASTER = "faster"
+    const val FAST = "fast"
+    const val MEDIUM = "medium"
+    const val SLOW = "slow"
+    const val SLOWER = "slower"
+    const val VERYSLOW = "veryslow"
+}
