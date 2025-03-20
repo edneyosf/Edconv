5641b53ce9359a48af5f7ea46f59a5ee4ea51e20
Edney Osf <edney.osf@gmail.com>
2025-03-20 09:35:41 -0300

    Refactoring Texts

--- src/main/kotlin/core/common/Texts.kt
+++ src/main/kotlin/core/common/Texts.kt
@@ -2,7 +2,9 @@ package core.common
 
 abstract class Texts(protected open val language: String) {
 
-    protected abstract val texts: Map<String, Map<String, String>>
+    protected abstract val texts: Map<String, Map<Long, String>>
+    protected abstract val pt: Map<Long, String>
+    protected abstract val en: Map<Long, String>
 
-    fun retrieve(key: String): String = texts[language]?.get(key) ?: "TEXT NOT FOUND"
+    fun retrieve(id: Long): String = texts[language]?.get(id) ?: "TEXT NOT FOUND"
 }
--- src/main/kotlin/features/home/texts/HomeTexts.kt
+++ src/main/kotlin/features/home/texts/HomeTexts.kt
@@ -1,18 +1,26 @@
 package features.home.texts
 
 import app.AppConfigs.LANGUAGE
+import app.AppLanguages.EN
 import app.AppLanguages.PT
 import core.common.Texts
 
 class HomeTexts(override val language: String = LANGUAGE): Texts(language) {
 
-    companion object {
-        const val TITLE_PICK_FILE_TEXT = "title_pick_file"
-        const val SELECT_FILE_TEXT = "select_file"
-    }
-
-    override val texts = mapOf(
-        PT to mapOf(TITLE_PICK_FILE_TEXT to "Escolha um arquivo"),
-        PT to mapOf(SELECT_FILE_TEXT to "Selecionar arquivo")
+    override val pt = mapOf(
+        TITLE_PICK_FILE_TEXT to "Escolha um arquivo",
+        SELECT_FILE_TEXT to "Selecionar arquivo"
     )
+
+    override val en = mapOf(
+        TITLE_PICK_FILE_TEXT to "Choose a file",
+        SELECT_FILE_TEXT to "Select file"
+    )
+
+    override val texts = mapOf(PT to pt, EN to en)
+
+    companion object {
+        const val TITLE_PICK_FILE_TEXT = 1L
+        const val SELECT_FILE_TEXT = 2L
+    }
 }
