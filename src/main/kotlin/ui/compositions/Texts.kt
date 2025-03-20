91cc8c4208b74db96272bba1ef19fa75cf69566c
Edney Osf <edney.osf@gmail.com>
2025-03-20 15:26:49 -0300

    Refactoring compositions

--- src/main/kotlin/ui/compositions/Texts.kt
+++ src/main/kotlin/ui/compositions/Texts.kt
@@ -1,6 +1,13 @@
 package ui.compositions
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.compositionLocalOf
 import core.common.Texts
 
-val texts = compositionLocalOf<Texts> { error("No value provided for texts") }
+val texts: Texts
+@Composable
+@ReadOnlyComposable
+get() = textsComp.current
+
+val textsComp = compositionLocalOf<Texts> { error("No value provided for texts") }
