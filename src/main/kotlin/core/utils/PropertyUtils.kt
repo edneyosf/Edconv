9d8c80b9c7466256ae83ad753fbec784313ee550
Edney Osf <edney.osf@gmail.com>
2025-03-21 16:53:28 -0300

    Moving binDir to DirUtils

--- src/main/kotlin/core/utils/PropertyUtils.kt
+++ src/main/kotlin/core/utils/PropertyUtils.kt
@@ -1,10 +1,7 @@
 package core.utils
 
-import java.io.File
-
 object PropertyUtils {
     val resourcesDir = System.getProperty("compose.application.resources.dir") ?: ""
     val userHomeDir = System.getProperty("user.home") ?: ""
-    val binDir = File(resourcesDir).parentFile.parentFile.parentFile.absolutePath + "/bin/"
     val version = System.getProperty("jpackage.app-version") ?: "version"
 }
