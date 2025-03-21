ca68d6bec9f4e0f39f20b9b4ea6f36f937963dfa
Edney Osf <edney.osf@gmail.com>
2025-03-21 06:46:18 -0300

    Refactoring MediaFormat

--- src/main/kotlin/edconv/common/MediaFormat.kt
+++ src/main/kotlin/edconv/common/MediaFormat.kt
@@ -1,22 +1,17 @@
 package edconv.common
 
-private const val AAC_ = "aac"
-private const val EAC3_ = "eac3"
-private const val H265_ = "h265"
-private const val AV1_ = "av1"
-
-enum class MediaFormat(val codec: String) {
-    AAC(AAC_),
-    EAC3(EAC3_),
-    H265(H265_),
-    AV1(AV1_);
+enum class MediaFormat(val codec: String, val text: String) {
+    AAC("aac", "AAC"),
+    EAC3("eac3", "E-AC3"),
+    H265("h265", "H.265"),
+    AV1("av1", "AV1");
 
     companion object {
         fun fromString(it: String) = when(it.lowercase()) {
-            AAC_ -> AAC
-            EAC3_ -> EAC3
-            H265_ -> H265
-            AV1_ -> AV1
+            "aac" -> AAC
+            "eac3" -> EAC3
+            "h265" -> H265
+            "av1" -> AV1
             else -> null
         }
     }
