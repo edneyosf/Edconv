b988c44d49ff5a175ebca61d0b41ca10dfcab322
Edney Osf <edney.osf@gmail.com>
2025-03-21 08:51:01 -0300

    Adding SVT and x265 text

--- src/main/kotlin/edconv/common/MediaFormat.kt
+++ src/main/kotlin/edconv/common/MediaFormat.kt
@@ -3,8 +3,8 @@ package edconv.common
 enum class MediaFormat(val codec: String, val text: String) {
     AAC("aac", "AAC"),
     EAC3("eac3", "E-AC3"),
-    H265("h265", "H.265"),
-    AV1("av1", "AV1");
+    H265("h265", "H.265 (x265)"),
+    AV1("av1", "AV1 (SVT)");
 
     companion object {
         fun fromString(it: String) = when(it.lowercase()) {
