c971ecdd11b6c375a06b05ba7b55acaacdd8f8bc
Edney Osf <edney.osf@gmail.com>
2025-03-21 16:54:41 -0300

    Refactoring configs

--- src/main/kotlin/core/Configs.kt
+++ src/main/kotlin/core/Configs.kt
@@ -3,14 +3,13 @@ package core
 object Configs {
     var title = ""
     var noAudioDefault = false
-    var channelsDefault = ""
     var vbrDefault = ""
-    var kbpsDefault = ""
+    var aacKbpsDefault = ""
+    var eac3KbpsDefault = ""
     var h265PresetDefault = ""
     var av1PresetDefault = ""
     var h265CrfDefault = 0
     var av1CrfDefault = 0
-    var bitDefault = ""
     var languageDefault = Languages.EN
     var outputFileDefault = ""
 }
