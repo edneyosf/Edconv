7cc0938f7983bdc82415ab0339556ce5b3130883
Edney Osf <edney.osf@gmail.com>
2025-03-26 07:38:31 -0300

    Adding getAll function to retrieve all channels

--- src/main/kotlin/edconv/common/Channels.kt
+++ src/main/kotlin/edconv/common/Channels.kt
@@ -3,5 +3,9 @@ package edconv.common
 enum class Channels(val value: String, val text: String) {
     STEREO(value = "2", text = "Stereo"),
     SURROUND_51(value = "6", text = "Surround 5.1"),
-    DOWNMIXING_SURROUND_51(value = "62", text = "Stereo (Surround 5.1)")
+    DOWNMIXING_SURROUND_51(value = "62", text = "Stereo (Surround 5.1)");
+
+    companion object {
+        fun getAll() = listOf(STEREO, DOWNMIXING_SURROUND_51, SURROUND_51)
+    }
 }
