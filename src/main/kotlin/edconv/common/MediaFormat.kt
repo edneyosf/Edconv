8a8cfb79ae63fb8f6e6ad00cf1eeb07bf945cb1c
Edney Osf <edney.osf@gmail.com>
2025-03-26 18:51:56 -0300

    Adding isVideo attribute to MediaFormat

--- src/main/kotlin/edconv/common/MediaFormat.kt
+++ src/main/kotlin/edconv/common/MediaFormat.kt
@@ -1,10 +1,10 @@
 package edconv.common
 
-enum class MediaFormat(val codec: String, val text: String) {
-    AAC(codec = "aac", text = "AAC"),
-    EAC3(codec = "eac3", text = "E-AC3"),
-    H265(codec = "h265", text = "H.265 (x265)"),
-    AV1(codec = "av1", text = "AV1 (SVT)");
+enum class MediaFormat(val codec: String, val text: String, val isVideo: Boolean) {
+    AAC(codec = "aac", text = "AAC", isVideo = false),
+    EAC3(codec = "eac3", text = "E-AC3", isVideo = false),
+    H265(codec = "h265", text = "H.265 (x265)", isVideo = true),
+    AV1(codec = "av1", text = "AV1 (SVT)", isVideo = true);
 
     fun toFileExtension() = when(this) {
         AAC -> "aac"
