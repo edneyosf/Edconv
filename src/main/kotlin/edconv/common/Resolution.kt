ac227646b4b8765565fe73e2553c8c682650a640
Edney Osf <edney.osf@gmail.com>
2025-03-27 08:21:18 -0300

    Refactoring arguments

--- src/main/kotlin/edconv/common/Resolution.kt
+++ src/main/kotlin/edconv/common/Resolution.kt
@@ -1,7 +1,11 @@
 package edconv.common
 
-enum class Resolution(val width: Int, val height: Int) {
-    P720(width = 1280, height = 720),
-    P1080(width = 1920, height = 1080),
-    P2160(width = 3840, height = 2160)
+enum class Resolution(val width: Int, val height: Int, val text: String) {
+    P720(width = 1280, height = 720, text = "720p"),
+    P1080(width = 1920, height = 1080, text = "1080p"),
+    P2160(width = 3840, height = 2160, text = "2160p");
+
+    companion object {
+        fun getAll() = entries.toList()
+    }
 }
