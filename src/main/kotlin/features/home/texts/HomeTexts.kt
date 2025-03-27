7879ee999e4cd5251d6630d238d95a3e91c2284a
Edney Osf <edney.osf@gmail.com>
2025-03-27 15:34:58 -0300

    Refactoring texts variable names

--- src/main/kotlin/features/home/texts/HomeTexts.kt
+++ src/main/kotlin/features/home/texts/HomeTexts.kt
@@ -15,58 +15,58 @@ inline val homeTexts: HomeTexts
 class HomeTexts(override val language: String): Texts(language) {
 
     override val pt = mapOf(
-        TITLE_PICK_FILE_TEXT to "Escolha um arquivo",
-        SELECT_FILE_TEXT to "Selecionar arquivo",
-        START_CONVERSION to "Iniciar",
-        STOP_CONVERSION to "Parar",
-        OUTPUT_FILE to "Salvar como",
-        FORMAT_INPUT to "Formato",
-        CHANNELS_INPUT to "Canais",
-        SAMPLE_RATE_INPUT to "Taxa de amostragem",
-        PIXEL_FORMAT_INPUT to "Formato de pixel",
-        RESOLUTION_INPUT to "Resolução",
-        NO_AUDIO_INPUT to "Sem áudio",
-        QUALITY_INPUT to "Qualidade constante",
-        BIT_RATE_INPUT to "Taxa de bits (kbps)",
-        AUDIO_MEDIA_TYPE to "Áudio",
-        VIDEO_MEDIA_TYPE  to "Vídeo",
+        TITLE_PICK_FILE_TXT to "Escolha um arquivo",
+        SELECT_FILE_TXT to "Selecionar arquivo",
+        START_CONVERSION_TXT to "Iniciar",
+        STOP_CONVERSION_TXT to "Parar",
+        OUTPUT_FILE_TXT to "Salvar como",
+        FORMAT_INPUT_TXT to "Formato",
+        CHANNELS_INPUT_TXT to "Canais",
+        SAMPLE_RATE_INPUT_TXT to "Taxa de amostragem",
+        PIXEL_FORMAT_INPUT_TXT to "Formato de pixel",
+        RESOLUTION_INPUT_TXT to "Resolução",
+        NO_AUDIO_INPUT_TXT to "Sem áudio",
+        QUALITY_INPUT_TXT to "Qualidade constante",
+        BIT_RATE_INPUT_TXT to "Taxa de bits (kbps)",
+        AUDIO_MEDIA_TYPE_TXT to "Áudio",
+        VIDEO_MEDIA_TYPE_TXT  to "Vídeo",
     )
 
     override val en = mapOf(
-        TITLE_PICK_FILE_TEXT to "Choose a file",
-        SELECT_FILE_TEXT to "Select file",
-        START_CONVERSION to "Start",
-        STOP_CONVERSION to "Stop",
-        OUTPUT_FILE to "Save as",
-        FORMAT_INPUT to "Format",
-        CHANNELS_INPUT to "Channels",
-        SAMPLE_RATE_INPUT to "Sample rate",
-        PIXEL_FORMAT_INPUT to "Pixel format",
-        RESOLUTION_INPUT to "Resolution",
-        NO_AUDIO_INPUT to "No audio",
-        QUALITY_INPUT to "Constant quality",
-        BIT_RATE_INPUT to "Bitrate (kbps)",
-        AUDIO_MEDIA_TYPE to "Audio",
-        VIDEO_MEDIA_TYPE  to "Video",
+        TITLE_PICK_FILE_TXT to "Choose a file",
+        SELECT_FILE_TXT to "Select file",
+        START_CONVERSION_TXT to "Start",
+        STOP_CONVERSION_TXT to "Stop",
+        OUTPUT_FILE_TXT to "Save as",
+        FORMAT_INPUT_TXT to "Format",
+        CHANNELS_INPUT_TXT to "Channels",
+        SAMPLE_RATE_INPUT_TXT to "Sample rate",
+        PIXEL_FORMAT_INPUT_TXT to "Pixel format",
+        RESOLUTION_INPUT_TXT to "Resolution",
+        NO_AUDIO_INPUT_TXT to "No audio",
+        QUALITY_INPUT_TXT to "Constant quality",
+        BIT_RATE_INPUT_TXT to "Bitrate (kbps)",
+        AUDIO_MEDIA_TYPE_TXT to "Audio",
+        VIDEO_MEDIA_TYPE_TXT  to "Video",
     )
 
     override val texts = mapOf(PT to pt, EN to en)
 
     companion object {
-        const val TITLE_PICK_FILE_TEXT = 1L
-        const val SELECT_FILE_TEXT = 2L
-        const val START_CONVERSION = 3L
-        const val STOP_CONVERSION = 4L
-        const val OUTPUT_FILE = 5L
-        const val FORMAT_INPUT = 6L
-        const val CHANNELS_INPUT = 7L
-        const val SAMPLE_RATE_INPUT = 8L
-        const val PIXEL_FORMAT_INPUT = 9L
-        const val RESOLUTION_INPUT = 10L
-        const val NO_AUDIO_INPUT = 11L
-        const val QUALITY_INPUT = 12L
-        const val BIT_RATE_INPUT = 13L
-        const val AUDIO_MEDIA_TYPE = 14L
-        const val VIDEO_MEDIA_TYPE = 15L
+        const val TITLE_PICK_FILE_TXT = 1L
+        const val SELECT_FILE_TXT = 2L
+        const val START_CONVERSION_TXT = 3L
+        const val STOP_CONVERSION_TXT = 4L
+        const val OUTPUT_FILE_TXT = 5L
+        const val FORMAT_INPUT_TXT = 6L
+        const val CHANNELS_INPUT_TXT = 7L
+        const val SAMPLE_RATE_INPUT_TXT = 8L
+        const val PIXEL_FORMAT_INPUT_TXT = 9L
+        const val RESOLUTION_INPUT_TXT = 10L
+        const val NO_AUDIO_INPUT_TXT = 11L
+        const val QUALITY_INPUT_TXT = 12L
+        const val BIT_RATE_INPUT_TXT = 13L
+        const val AUDIO_MEDIA_TYPE_TXT = 14L
+        const val VIDEO_MEDIA_TYPE_TXT = 15L
     }
 }
