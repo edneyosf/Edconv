c562240349c5890f881bbdf3d4f752750275953b
Edney Osf <edney.osf@gmail.com>
2025-03-31 13:38:12 -0300

    Adding value ffmpeg to PixelFormat

--- src/main/kotlin/edconv/common/PixelFormat.kt
+++ src/main/kotlin/edconv/common/PixelFormat.kt
@@ -1,8 +1,8 @@
 package edconv.common
 
 enum class PixelFormat(val value: String, val text: String) {
-    BIT_8(value = "8", text = "8-bit"),
-    BIT_10(value = "10", text = "10-bit");
+    BIT_8(value = "yuv420p", text = "8-bit"),
+    BIT_10(value = "yuv420p10le", text = "10-bit");
 
     companion object {
         fun getAll() = entries.toList()
