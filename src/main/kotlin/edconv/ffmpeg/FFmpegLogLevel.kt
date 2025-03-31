7a0c644465ac19ae04b266d5ddc9d80a6f1d7510
Edney Osf <edney.osf@gmail.com>
2025-03-31 13:34:18 -0300

    Adding FFmpeg log level

---
+++ src/main/kotlin/edconv/ffmpeg/FFmpegLogLevel.kt
@@ -0,0 +1,13 @@
+package edconv.ffmpeg
+
+object FFmpegLogLevel {
+    const val QUIET = "quiet"
+    const val PANIC = "panic"
+    const val FATAL = "fatal"
+    const val ERROR = "error"
+    const val WARNING = "warning"
+    const val INFO = "info"
+    const val VERBOSE = "verbose"
+    const val DEBUG = "debug"
+    const val TRACE = "trace"
+}
