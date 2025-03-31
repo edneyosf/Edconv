3ce52657da91fa1308318a9bdf951ba0a3c9c2a2
Edney Osf <edney.osf@gmail.com>
2025-03-31 13:48:22 -0300

    Renaming kbps to bitrate and format to codec

--- src/main/kotlin/features/home/events/HomeEvent.kt
+++ src/main/kotlin/features/home/events/HomeEvent.kt
@@ -5,10 +5,10 @@ import edconv.common.*
 sealed interface HomeEvent {
     data class SetInputFile(val inputFile: String): HomeEvent
     data class SetOutputFile(val outputFile: String): HomeEvent
-    data class SetFormat(val format: MediaFormat?): HomeEvent
+    data class SetCodec(val codec: Codec?): HomeEvent
     data class SetChannels(val channels: Channels?): HomeEvent
     data class SetVbr(val vbr: Int?): HomeEvent
-    data class SetKbps(val kbps: Kbps): HomeEvent
+    data class SetBitrate(val bitrate: Bitrate): HomeEvent
     data class SetSampleRate(val sampleRate: SampleRate?): HomeEvent
     data class SetPreset(val preset: String): HomeEvent
     data class SetCrf(val crf: Int): HomeEvent
