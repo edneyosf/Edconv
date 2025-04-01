4f8669b3697f7083adfae3a227217b07b0ad4c55
Edney Osf <edney.osf@gmail.com>
2025-04-01 08:44:03 -0300

    Adding MediaUtils to get resolution, duration, size and media type.

--- src/main/kotlin/core/utils/MediaUtils.kt
+++ src/main/kotlin/core/utils/MediaUtils.kt
@@ -2,74 +2,115 @@ package core.utils
 
 import core.Configs
 import edconv.common.MediaType
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.io.BufferedReader
 import java.io.File
 import java.io.InputStreamReader
 
 object MediaUtils {
-    fun getVideoResolution(file: File): Pair<Int, Int> {
+
+    /**
+     * Command: ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=p=0 <file_path>
+     * Result model: "1920,1080"
+     */
+    suspend fun getVideoResolution(file: File): Pair<Int, Int>? = withContext(context = Dispatchers.IO) {
+        var resolution: Pair<Int, Int>? = null
         val command = arrayOf(
             Configs.ffprobePath,
             "-v", "error",
             "-select_streams", "v:0",
             "-show_entries", "stream=width,height",
-            "-of", "csv=s=x:p=0",
+            "-of", "csv=p=0",
             file.absolutePath
         )
-        val process = ProcessBuilder(*command)
-            .redirectErrorStream(true)
-            .start()
 
-        val inputStream = InputStreamReader(process.inputStream)
-        val reader = BufferedReader(inputStream)
-        val data = reader.readLine()?.split("x")
-        val resolution = data?.map { it.toInt() } ?: listOf(0, 0)
-        val width = resolution.getOrElse(index = 0, defaultValue = { 0 })
-        val height = resolution.getOrElse(index = 1, defaultValue = { 0 })
+        try {
+            val reader = getReader(command)
+            val line = reader.readLine()!!
+            val data = line.trim().split(",")
+            val width = data.first().toInt()
+            val height = data.last().toInt()
 
-        return Pair(width, height)
+            resolution = Pair(width, height)
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return@withContext resolution
     }
 
-    fun getDuration(file: File): Long {
+    /**
+     * Command: ffprobe -v error -show_entries format=duration -of csv=p=0 <file_path>
+     * Result model: "1382.255000"
+     */
+    suspend fun getDuration(file: File): Long? = withContext(context = Dispatchers.IO) {
+        var duration: Long? = null
         val command = arrayOf(
             Configs.ffprobePath,
             "-v", "error",
             "-show_entries", "format=duration",
-            "-of", "default=noprint_wrappers=1:nokey=1",
+            "-of", "csv=p=0",
             file.absolutePath
         )
-        val process = ProcessBuilder(*command)
-            .redirectErrorStream(true)
-            .start()
 
-        val inputStream = InputStreamReader(process.inputStream)
-        val reader = BufferedReader(inputStream)
-        val durationInSeconds = reader.readLine()?.toDoubleOrNull() ?: 0.0
+        try {
+            val reader = getReader(command)
+            val line = reader.readLine()!! // 1382.255000
+            val durationInSeconds = line.trim().toDouble()
 
-        return (durationInSeconds * 1000).toLong()
+            duration = (durationInSeconds * 1000).toLong()
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return@withContext duration
     }
 
-    fun getType(file: File): MediaType {
-        val command = listOf(
+    /**
+     * Command: ffprobe -v error -show_entries stream=codec_type -of csv=p=0 <file_path>
+     * Result model:
+     * "video
+     *  audio
+     *  subtitle"
+     */
+    suspend fun getType(file: File): MediaType? = withContext(context = Dispatchers.IO) {
+        var type: MediaType? = null
+        val command = arrayOf(
             Configs.ffprobePath,
             "-v", "error",
             "-show_entries", "stream=codec_type",
             "-of", "csv=p=0",
             file.absolutePath
         )
-        val process = ProcessBuilder(command)
-            .redirectErrorStream(true)
-            .start()
 
-        val reader = BufferedReader(InputStreamReader(process.inputStream))
-        val output =  reader.readLines().map { it.trim() }
+        try {
+            val reader = getReader(command)
+            val lines = reader.readLines()
+            val output =  lines.map { it.trim() }
 
-        return when {
-            "video" in output -> MediaType.VIDEO
-            "audio" in output -> MediaType.AUDIO
-            else -> MediaType.UNKNOWN
+            type = when {
+                "video" in output -> MediaType.VIDEO
+                "audio" in output -> MediaType.AUDIO
+                else -> MediaType.UNKNOWN
+            }
         }
+        catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return@withContext type
     }
 
     fun getSize(file: File) = file.length()
+
+    private fun getReader(command: Array<String>): BufferedReader {
+        val process = ProcessBuilder(*command)
+            .redirectErrorStream(true)
+            .start()
+
+        return BufferedReader(InputStreamReader(process.inputStream))
+    }
 }
