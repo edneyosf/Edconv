716be863cd783eb54841cef44a544830e2a73481
Edney Osf <edney.osf@gmail.com>
2025-04-01 08:55:51 -0300

    Removing unused extensions

--- src/main/kotlin/edconv/core/utils/MediaInfoExtension.kt
+++
@@ -1,7 +0,0 @@
-package edconv.core.utils
-
-import edconv.core.EdconvConfigs.MEDIA_INFO
-
-fun String.isMediaInfo() = this.contains(MEDIA_INFO)
-
-fun String.retrieveMediaInfoJson() = this.substringAfter(MEDIA_INFO)
--- src/main/kotlin/edconv/core/utils/StatusExtension.kt
+++
@@ -1,8 +0,0 @@
-package edconv.core.utils
-
-import edconv.core.EdconvConfigs.STATUS_COMPLETE
-import edconv.core.EdconvConfigs.STATUS_ERROR
-
-fun String.isStatusComplete() = this.contains(STATUS_COMPLETE)
-
-fun String.isStatusError() = this.contains(STATUS_ERROR)
