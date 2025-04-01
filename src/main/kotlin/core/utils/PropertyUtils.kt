a228b11a0c041275fa5da64a4874eda76d06c399
Edney Osf <edney.osf@gmail.com>
2025-04-01 07:25:24 -0300

    version in PropertyUtils nullable

--- src/main/kotlin/core/utils/PropertyUtils.kt
+++ src/main/kotlin/core/utils/PropertyUtils.kt
@@ -3,5 +3,5 @@ package core.utils
 object PropertyUtils {
     val resourcesDir = System.getProperty("compose.application.resources.dir") ?: ""
     val userHomeDir = System.getProperty("user.home") ?: ""
-    val version = System.getProperty("jpackage.app-version")
+    val version: String? = System.getProperty("jpackage.app-version")
 }
