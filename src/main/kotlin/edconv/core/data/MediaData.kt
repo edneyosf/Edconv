1234ff40239345c9e4bcc0e44b395d3e6479bd68
Edney Osf <edney.osf@gmail.com>
2025-04-01 08:53:57 -0300

    Refactoring data

--- src/main/kotlin/edconv/core/data/MediaData.kt
+++ src/main/kotlin/edconv/core/data/MediaData.kt
@@ -2,13 +2,12 @@ package edconv.core.data
 
 import edconv.common.MediaType
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
 
 @Serializable
 data class MediaData(
-    val path: String, val type: MediaType, val duration: Long, val resolution: Pair<Int, Int>? = null, val size: Long
-) {
-    companion object {
-        fun fromJsonString(json: String): MediaData = Json.decodeFromString(json)
-    }
-}
+    val path: String,
+    val type: MediaType,
+    val duration: Long,
+    val resolution: Pair<Int, Int>? = null,
+    val size: Long
+)
--- src/main/kotlin/edconv/core/data/ProgressData.kt
+++ src/main/kotlin/edconv/core/data/ProgressData.kt
@@ -1,16 +1,11 @@
 package edconv.core.data
 
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
 
 @Serializable
 data class ProgressData(
     val size: String,
-    val time: Long,   // Tempo processado (HH:MM:SS.ms)
-    val bitrate: String, // Bitrate em kbps
-    val speed: String // Velocidade de processamento
-) {
-    companion object {
-        fun fromJsonString(json: String): ProgressData = Json.decodeFromString(json)
-    }
-}
+    val time: Long, // milliseconds
+    val bitrate: String,
+    val speed: String
+)
