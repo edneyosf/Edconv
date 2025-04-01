5682f4cdc68c945190b50a911f60b2dcbaea5a95
Edney Osf <edney.osf@gmail.com>
2025-04-01 07:22:46 -0300

    Removing binDir

--- src/main/kotlin/core/utils/DirUtils.kt
+++ src/main/kotlin/core/utils/DirUtils.kt
@@ -1,9 +1,7 @@
 package core.utils
 
-import core.utils.PropertyUtils.resourcesDir
 import java.io.File
 
 object DirUtils {
-    val binDir = File(resourcesDir).parentFile.parentFile.parentFile.absolutePath + "/bin/"
     val outputDir = PropertyUtils.userHomeDir + File.separator + "Edconv" + File.separator
 }
