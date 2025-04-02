41505f6685504c3426efd83bbf361264e7e0561b
Edney Osf <edney.osf@gmail.com>
2025-04-02 09:59:07 -0300

    Refactoring Edconv config

--- src/main/kotlin/edconv/core/EdconvConfigs.kt
+++ src/main/kotlin/edconv/core/EdconvConfigs.kt
@@ -1,7 +1,6 @@
 package edconv.core
 
 object EdconvConfigs {
-    const val FFMPEG = "ffmpeg"
-    const val FFPROBE = "ffprobe"
     const val TIME_PATTERN = "HH:mm:ss.SS"
+    const val PROGRESS_PATTERN = "size=\\s*(\\d+KiB)\\s+time=(\\d+:\\d+:\\d+\\.\\d+)\\s+bitrate=\\s*([\\d.]+kbits/s)\\s+speed=\\s*([\\d.]+x)"
 }
