a807ada2918e3a0512f6438be2d6b218d766ffcc
Edney Osf <edney.osf@gmail.com>
2025-04-03 18:03:36 -0300

    Adding content type

---
+++ src/main/kotlin/edconv/core/data/ContentTypeData.kt
@@ -0,0 +1,6 @@
+package edconv.core.data
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ContentTypeData(val audio: Boolean = false, val video: Boolean = false, val subtitle: Boolean = false)
