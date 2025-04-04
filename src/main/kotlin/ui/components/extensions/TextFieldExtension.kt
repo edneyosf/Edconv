0008219cb7f421b49e9fa4ac5ce0e73db039da45
Edney Osf <edney.osf@gmail.com>
2025-04-04 06:52:20 -0300

    Refactoring TextFieldExtension

--- src/main/kotlin/ui/components/extensions/TextFieldExtension.kt
+++ src/main/kotlin/ui/components/extensions/TextFieldExtension.kt
@@ -3,19 +3,18 @@ package ui.components.extensions
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TextFieldColors
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 
 @Composable
 fun TextFieldColors.custom(darkTheme: Boolean = isSystemInDarkTheme()): TextFieldColors {
     return if(darkTheme) {
-        TextFieldDefaults.colors().copy(
+        this.copy(
             unfocusedIndicatorColor = MaterialTheme.colorScheme.surfaceContainer,
             disabledIndicatorColor = MaterialTheme.colorScheme.surfaceContainer
         )
     }
     else {
-        TextFieldDefaults.colors().copy(
+        this.copy(
             unfocusedIndicatorColor = MaterialTheme.colorScheme.surfaceDim,
             disabledIndicatorColor = MaterialTheme.colorScheme.surfaceDim
         )
