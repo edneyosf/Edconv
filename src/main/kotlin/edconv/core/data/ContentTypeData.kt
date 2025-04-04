3da6882ac8b95a224ac983fadfd0aaa1130ed36f
Edney Osf <edney.osf@gmail.com>
2025-04-04 06:52:01 -0300

    Refactoring content type data

--- src/main/kotlin/edconv/core/data/ContentTypeData.kt
+++ src/main/kotlin/edconv/core/data/ContentTypeData.kt
@@ -3,4 +3,8 @@ package edconv.core.data
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ContentTypeData(val audio: Boolean = false, val video: Boolean = false, val subtitle: Boolean = false)
+data class ContentTypeData(
+    val audio: Boolean = false,
+    val video: Boolean = false,
+    val subtitle: Boolean = false
+)
