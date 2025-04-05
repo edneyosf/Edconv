3793747468478da34bdacfc25e54a65506fde946
Edney Osf <edney.osf@gmail.com>
2025-04-05 11:38:17 -0300

    Adding windows support

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,7 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
-//val binDir = File(project.rootDir, "bin")
-val appName = "Edconv"
 val appVersion = "1.0.0"
-//val core = "edconv"
-//val ffmpeg = "ffmpeg-ed"
-//val ffprobe = "ffprobe-ed"
 
 group = "com.radiuere"
 version = "${appVersion}-SNAPSHOT"
@@ -43,8 +38,8 @@ compose.desktop {
         mainClass = "MainKt"
 
         nativeDistributions {
-            targetFormats(TargetFormat.Deb)
-            packageName = appName
+            targetFormats(TargetFormat.Deb, TargetFormat.Exe)
+            packageName = "Edconv"
             packageVersion = appVersion
             description = "A multimedia converter for movies, tv shows and music"
             copyright = "© 2025 Radiuere. All rights reserved."
@@ -54,75 +49,9 @@ compose.desktop {
             linux {
                 iconFile.set(appResourcesRootDir.file("linux/icon.png"))
             }
-        }
-    }
-}
-
-/*afterEvaluate {
-    tasks.named("run") {
-        doFirst {
-            val buildBinDir = file("${layout.buildDirectory.get()}/bin")
-            val coreFile = File(binDir, core)
-            val coreBuildFile = File(buildBinDir, core)
-            val ffmpegFile = File(binDir, ffmpeg)
-            val ffmpegBuildFile = File(buildBinDir, ffmpeg)
-            val ffprobeFile = File(binDir, ffprobe)
-            val ffprobeBuildFile = File(buildBinDir, ffprobe)
-
-            if(!buildBinDir.exists()) buildBinDir.mkdirs()
-
-            coreFile.copyTo(coreBuildFile, overwrite = true)
-            ffmpegFile.copyTo(ffmpegBuildFile, overwrite = true)
-            ffprobeFile.copyTo(ffprobeBuildFile, overwrite = true)
-
-            coreBuildFile.setExecutable(true)
-            ffmpegBuildFile.setExecutable(true)
-            ffprobeBuildFile.setExecutable(true)
-        }
-    }
-}
-
-afterEvaluate {
-    tasks.named("packageDeb") {
-        doLast {
-            val debDir = file("${layout.buildDirectory.get()}/compose/binaries/main/deb")
-            val debFile = File(debDir, "${appName.lowercase()}_${appVersion}_amd64.deb")
-
-            if (debFile.exists()) {
-                val sourceDir = File(debDir, "source")
-                val debianDir = File(sourceDir, "DEBIAN")
-                val debianFile = File(debDir, "${appName.lowercase()}.deb")
-                val sourceBinDir = File(sourceDir, "opt/${appName.lowercase()}/bin")
-
-                sourceDir.deleteRecursively()
-                if (!debianDir.exists()) debianDir.mkdirs()
-                debFile.renameTo(debianFile)
-
-                exec { commandLine("dpkg-deb", "-e", debianFile.absolutePath, debianDir.absolutePath) }
-                exec { commandLine("dpkg", "-x", debianFile.absolutePath, sourceDir.absolutePath) }
-
-                val coreFile = File(binDir, core)
-                val coreSourceFile = File(sourceBinDir, core)
-                val ffmpegFile = File(binDir, ffmpeg)
-                val ffmpegSourceFile = File(sourceBinDir, ffmpeg)
-                val ffprobeFile = File(binDir, ffprobe)
-                val ffprobeSourceFile = File(sourceBinDir, ffprobe)
-
-                coreFile.copyTo(coreSourceFile, overwrite = true)
-                ffmpegFile.copyTo(ffmpegSourceFile, overwrite = true)
-                ffprobeFile.copyTo(ffprobeSourceFile, overwrite = true)
-                coreSourceFile.setExecutable(true)
-                ffmpegSourceFile.setExecutable(true)
-                ffprobeSourceFile.setExecutable(true)
-
-                exec { commandLine("dpkg", "--build", sourceDir.absolutePath, debFile.absolutePath) }
-
-                debianFile.delete()
-                sourceDir.deleteRecursively()
-            }
-            else {
-                throw Exception("${debFile.name} does not exist")
+            windows {
+                iconFile.set(appResourcesRootDir.file("windows/icon.ico"))
             }
         }
     }
-}*/
+}
