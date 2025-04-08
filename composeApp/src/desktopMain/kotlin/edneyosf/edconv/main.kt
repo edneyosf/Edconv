6af279aa9ec4a5681bb9880d83d61ac04e3b159b
Edney Osf <edney.osf@gmail.com>
2025-04-08 11:18:33 -0300

    Test windows icon

--- composeApp/src/desktopMain/kotlin/edneyosf/edconv/main.kt
+++ composeApp/src/desktopMain/kotlin/edneyosf/edconv/main.kt
@@ -8,6 +8,11 @@ import edneyosf.edconv.app.AppConfigs.MIN_WINDOW_WIDTH
 import edneyosf.edconv.app.AppConfigs.MIN_WINDOW_HEIGHT
 import edneyosf.edconv.core.utils.DirUtils
 import java.awt.Dimension
+import java.awt.Image
+import java.io.File
+import java.io.InputStream
+import javax.imageio.ImageIO
+import javax.swing.SwingUtilities
 
 fun main() {
     setConfigs()
@@ -21,6 +26,27 @@ fun main() {
                 App()
             }
         )
+        setAppIcon("resources/icon.png")
+    }
+}
+
+fun setAppIcon(path: String) {
+    val image: Image? = try {
+        val file = File(path)
+        println("aaa: "+file.absolutePath)
+        ImageIO.read(file)
+    } catch (e: Exception) {
+        println("error: "+e.message)
+        null
+    }
+
+    if (image != null) {
+        // Espera a janela estar pronta
+        println("huhu!")
+        SwingUtilities.invokeLater {
+            val frames = java.awt.Frame.getFrames()
+            frames.forEach { it.iconImage = image }
+        }
     }
 }
 
