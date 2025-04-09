317f6576c831b5aa28afcdf81574aa5aa884aaf2
Edney Osf <edney.osf@gmail.com>
2025-04-09 11:44:18 -0300

    Adding Selector component

--- composeApp/src/desktopMain/kotlin/edneyosf/edconv/ui/components/Selector.kt
+++ composeApp/src/desktopMain/kotlin/edneyosf/edconv/ui/components/Selector.kt
@@ -1,10 +1,16 @@
 package edneyosf.edconv.ui.components
 
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import edneyosf.edconv.ui.components.extensions.custom
+import edneyosf.edconv.ui.compositions.dimens
+import edneyosf.edconv.ui.previews.DarkPreview
+import edneyosf.edconv.ui.previews.LightPreview
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -26,7 +32,6 @@ fun Selector(
             colors = TextFieldDefaults.colors().custom(),
             modifier = Modifier.menuAnchor(MenuAnchorType.PrimaryNotEditable)
         )
-
         ExposedDropdownMenu(
             expanded = expanded,
             onDismissRequest = { onExpanded(false) },
@@ -34,3 +39,30 @@ fun Selector(
         )
     }
 }
+
+@Composable
+private fun DefaultPreview() {
+    Column(verticalArrangement = Arrangement.spacedBy(dimens.xs)) {
+        Selector(
+            text = "Sample",
+            label = "Sample",
+            expanded = true,
+            onExpanded = { }
+        ) {}
+        Selector(
+            text = "Sample",
+            label = "Sample",
+            enabled = false,
+            expanded = true,
+            onExpanded = { }
+        ) {}
+    }
+}
+
+@Preview
+@Composable
+private fun Light() = LightPreview { DefaultPreview() }
+
+@Preview
+@Composable
+private fun Dark() = DarkPreview { DefaultPreview() }
