d982095bcadaa984583f8c8f34e51ed8f93d0892
Edney Osf <edney.osf@gmail.com>
2025-04-09 07:29:46 -0300

    Adding windows icon

--- composeApp/src/desktopMain/kotlin/edneyosf/edconv/main.kt
+++ composeApp/src/desktopMain/kotlin/edneyosf/edconv/main.kt
@@ -2,17 +2,15 @@ package edneyosf.edconv
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import edconv.composeapp.generated.resources.Res
+import edconv.composeapp.generated.resources.icon
 import edneyosf.edconv.app.App
 import edneyosf.edconv.app.AppConfigs
 import edneyosf.edconv.app.AppConfigs.MIN_WINDOW_WIDTH
 import edneyosf.edconv.app.AppConfigs.MIN_WINDOW_HEIGHT
 import edneyosf.edconv.core.utils.DirUtils
+import org.jetbrains.compose.resources.painterResource
 import java.awt.Dimension
-import java.awt.Image
-import java.io.File
-import java.io.InputStream
-import javax.imageio.ImageIO
-import javax.swing.SwingUtilities
 
 fun main() {
     setConfigs()
@@ -21,32 +19,12 @@ fun main() {
         Window(
             title = AppConfigs.NAME,
             onCloseRequest = ::exitApplication,
+            icon = painterResource(Res.drawable.icon),
             content = {
                 window.minimumSize = Dimension(MIN_WINDOW_WIDTH, MIN_WINDOW_HEIGHT)
                 App()
             }
         )
-        setAppIcon("resources/icon.png")
-    }
-}
-
-fun setAppIcon(path: String) {
-    val image: Image? = try {
-        val file = File(path)
-        println("aaa: "+file.absolutePath)
-        ImageIO.read(file)
-    } catch (e: Exception) {
-        println("error: "+e.message)
-        null
-    }
-
-    if (image != null) {
-        // Espera a janela estar pronta
-        println("huhu!")
-        SwingUtilities.invokeLater {
-            val frames = java.awt.Frame.getFrames()
-            frames.forEach { it.iconImage = image }
-        }
     }
 }
 
