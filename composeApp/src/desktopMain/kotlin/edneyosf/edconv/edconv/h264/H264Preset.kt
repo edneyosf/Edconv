d90a8a629940ae0ea717c2fc22c662e54d0b02c5
Edney Osf <edney.osf@gmail.com>
2025-04-14 15:26:27 -0300

    Adding functions valueByIndex and indexByValue to presets

--- composeApp/src/desktopMain/kotlin/edneyosf/edconv/edconv/h264/H264Preset.kt
+++ composeApp/src/desktopMain/kotlin/edneyosf/edconv/edconv/h264/H264Preset.kt
@@ -1,20 +1,19 @@
 package edneyosf.edconv.edconv.h264
 
-enum class H264Preset(val id: Int, val value: String) {
-    ULTRAFAST(id = 0, value = "ultrafast"),
-    SUPERFAST(id = 1, value = "superfast"),
-    VERYFAST(id = 2, value = "veryfast"),
-    FASTER(id = 3, value = "faster"),
-    FAST(id = 4, value = "fast"),
-    MEDIUM(id = 5, value = "medium"),
-    SLOW(id = 6, value = "slow"),
-    SLOWER(id = 7, value = "slower"),
-    VERYSLOW(id = 8, value = "veryslow");
+enum class H264Preset(val value: String) {
+    ULTRAFAST(value = "ultrafast"),
+    SUPERFAST(value = "superfast"),
+    VERYFAST(value = "veryfast"),
+    FASTER(value = "faster"),
+    FAST(value = "fast"),
+    MEDIUM(value = "medium"),
+    SLOW(value = "slow"),
+    SLOWER(value = "slower"),
+    VERYSLOW(value = "veryslow"),
+    PLACEBO(value = "placebo");
 
     companion object {
-        const val MIN_ID = 0
-        const val MAX_ID = 8
-
-        fun fromId(id: Int) = entries.find { it.id == id }
+        fun valueByIndex(index: Int) = entries.find { it.ordinal == index }?.value
+        fun indexByValue(value: String) = entries.find { it.value == value }?.ordinal
     }
 }
