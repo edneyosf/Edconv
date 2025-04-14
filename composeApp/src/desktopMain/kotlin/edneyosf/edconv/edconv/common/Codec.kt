b6eff7d463a7037a693c60a672ab8056b8f7b03d
Edney Osf <edney.osf@gmail.com>
2025-04-14 08:15:54 -0300

    Renaming compression to compressions

--- composeApp/src/desktopMain/kotlin/edneyosf/edconv/edconv/common/Codec.kt
+++ composeApp/src/desktopMain/kotlin/edneyosf/edconv/edconv/common/Codec.kt
@@ -1,7 +1,7 @@
 package edneyosf.edconv.edconv.common
 
 enum class Codec(
-    val value: String, val text: String, val mediaType: MediaType, val compression: List<CompressionType>,
+    val value: String, val text: String, val mediaType: MediaType, val compressions: List<CompressionType>,
     val defaultCRF: Int? = null, val minCRF: Int? = null, val maxCRF: Int? = null, val defaultVBR: Int? = null,
     val minVBR: Int? = null, val maxVBR: Int? = null
 ) {
@@ -9,13 +9,13 @@ enum class Codec(
         value = "aac",
         text = "AAC",
         mediaType = MediaType.AUDIO,
-        compression = listOf(CompressionType.CBR),
+        compressions = listOf(CompressionType.CBR),
     ),
     AAC_FDK(
         value = "libfdk_aac",
         text = "AAC (FDK)",
         mediaType = MediaType.AUDIO,
-        compression = listOf(CompressionType.VBR, CompressionType.CBR),
+        compressions = listOf(CompressionType.VBR, CompressionType.CBR),
         defaultVBR = 5,
         minVBR = 1,
         maxVBR = 5
@@ -24,31 +24,31 @@ enum class Codec(
         value = "libopus",
         text = "Opus",
         mediaType = MediaType.AUDIO,
-        compression = listOf(CompressionType.CBR)
+        compressions = listOf(CompressionType.CBR)
     ),
     AC3(
         value = "ac3",
         text = "AC3", mediaType = MediaType.AUDIO,
-        compression = listOf(CompressionType.CBR)
+        compressions = listOf(CompressionType.CBR)
     ),
     EAC3(
         value = "eac3",
         text = "EAC3",
         mediaType = MediaType.AUDIO,
-        compression = listOf(CompressionType.CBR)
+        compressions = listOf(CompressionType.CBR)
     ),
     FLAC(
         value = "flac",
         text = "FLAC",
         mediaType = MediaType.AUDIO,
-        compression = emptyList()
+        compressions = emptyList()
     ),
 
     H264(
         value = "libx264",
         text = "H.264 (x264)",
         mediaType = MediaType.VIDEO,
-        compression = listOf(CompressionType.CRF, CompressionType.CBR),
+        compressions = listOf(CompressionType.CRF, CompressionType.CBR),
         defaultCRF = 20,
         minCRF = 0,
         maxCRF = 51
@@ -57,7 +57,7 @@ enum class Codec(
         value = "libx265",
         text = "H.265 (x265)",
         mediaType = MediaType.VIDEO,
-        compression = listOf(CompressionType.CRF, CompressionType.CBR),
+        compressions = listOf(CompressionType.CRF, CompressionType.CBR),
         defaultCRF = 21,
         minCRF = 0,
         maxCRF = 51
@@ -66,7 +66,7 @@ enum class Codec(
         value = "libvpx-vp9",
         text = "VP9",
         mediaType = MediaType.VIDEO,
-        compression = listOf(CompressionType.CRF, CompressionType.CBR),
+        compressions = listOf(CompressionType.CRF, CompressionType.CBR),
         defaultCRF = 19,
         minCRF = 0,
         maxCRF = 63
@@ -75,7 +75,7 @@ enum class Codec(
         value = "libsvtav1",
         text = "AV1 (SVT)",
         mediaType = MediaType.VIDEO,
-        compression = listOf(CompressionType.CRF, CompressionType.CBR),
+        compressions = listOf(CompressionType.CRF, CompressionType.CBR),
         defaultCRF = 25,
         minCRF = 0,
         maxCRF = 63
