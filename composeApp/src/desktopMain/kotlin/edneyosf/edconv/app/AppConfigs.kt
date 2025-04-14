cec96d885ac91bb741020d17a3a88cf263457d53
Edney Osf <edney.osf@gmail.com>
2025-04-14 17:16:38 -0300

    Minimum windows size to 1100

--- composeApp/src/desktopMain/kotlin/edneyosf/edconv/app/AppConfigs.kt
+++ composeApp/src/desktopMain/kotlin/edneyosf/edconv/app/AppConfigs.kt
@@ -6,7 +6,7 @@ import edneyosf.edconv.edconv.ffmpeg.FFmpegLogLevel
 object AppConfigs {
     var outputDefault = ""
     const val NAME = "Edconv"
-    const val MIN_WINDOW_WIDTH = 1250
+    const val MIN_WINDOW_WIDTH = 1100
     const val MIN_WINDOW_HEIGHT = 730
     const val LANGUAGE = Languages.EN
     const val FFMPEG_LOG_LEVEL = FFmpegLogLevel.VERBOSE
