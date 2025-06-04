d5bf8b66ec4ae29a0ada05201266ff3a9bfd0c97
Edney Osf <edney.osf@gmail.com>
2025-06-04 14:48:35 -0300

    Renaming VMAF classes

--- composeApp/src/desktopMain/kotlin/edneyosf/edconv/features/vmaf/events/VMAFEvent.kt
+++
@@ -1,9 +0,0 @@
-package edneyosf.edconv.features.vmaf.events
-
-import edneyosf.edconv.features.common.models.InputMedia
-
-interface VMAFEvent {
-    fun refresh(newInput: InputMedia) = Unit
-    fun pickSourceFile(title: String) = Unit
-    fun pickModelFile(title: String) = Unit
-}
