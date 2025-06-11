3e295b6fe4b1aa1a873cee25ff0319da761c33c2
Edney Osf <edney.osf@gmail.com>
2025-06-11 15:59:28 -0300

    VMAF state complete

--- composeApp/src/desktopMain/kotlin/edneyosf/edconv/features/vmaf/states/VmafStatusState.kt
+++ composeApp/src/desktopMain/kotlin/edneyosf/edconv/features/vmaf/states/VmafStatusState.kt
@@ -6,6 +6,12 @@ sealed interface VmafStatusState {
     data object Initial: VmafStatusState
     data object Loading: VmafStatusState
     data class Progress(val percentage: Float, val speed: String): VmafStatusState
-    data object Complete: VmafStatusState
     data class Failure(val error: Error): VmafStatusState
+
+    data class Complete(
+        val startTime: String,
+        val finishTime: String,
+        val duration: String,
+        val score: String
+    ): VmafStatusState
 }
