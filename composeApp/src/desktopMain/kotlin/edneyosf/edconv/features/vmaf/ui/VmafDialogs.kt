7ef6816bff3ab60bbd32fd61749b0219c28b5857
Edney Osf <edney.osf@gmail.com>
2025-07-09 15:45:18 -0300

    Windowed media information

--- composeApp/src/desktopMain/kotlin/edneyosf/edconv/features/vmaf/ui/VmafDialogs.kt
+++ composeApp/src/desktopMain/kotlin/edneyosf/edconv/features/vmaf/ui/VmafDialogs.kt
@@ -1,9 +1,7 @@
 package edneyosf.edconv.features.vmaf.ui
 
 import androidx.compose.runtime.Composable
-import edneyosf.edconv.features.mediainfo.MediaInfoDialog
 import edneyosf.edconv.features.vmaf.VmafEvent
-import edneyosf.edconv.features.vmaf.states.VmafDialogState.*
 import edneyosf.edconv.features.vmaf.states.VmafState
 import edneyosf.edconv.features.vmaf.states.VmafStatusState.*
 
@@ -24,17 +22,6 @@ fun VmafState.Dialogs(event: VmafEvent) {
                     finishTime = finishTime,
                     duration = duration,
                     onFinish = { event.setStatus(Initial) }
-                )
-            }
-            else -> Unit
-        }
-    }
-
-    dialog.run {
-        when (this) {
-            is MediaInfo -> {
-                inputMedia.MediaInfoDialog(
-                    onFinish = { event.setDialog(dialog = None) }
                 )
             }
             else -> Unit
