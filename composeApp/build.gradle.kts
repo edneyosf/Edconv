01a4cb382a747d69abb74332b744b4c2677d497b
Edney Osf <edney.osf@gmail.com>
2025-10-20 09:43:37 -0300

    Feat: Removing Hot Reload Gradle Plugin

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -10,7 +10,6 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
-    alias(libs.plugins.composeHotReload)
     alias(libs.plugins.serialization)
     alias(libs.plugins.flatpakGenerator)
 }
