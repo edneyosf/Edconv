95aeaf226f0a27052f907e347b50227c071159a6
Edney Osf <edney.osf@gmail.com>
2025-10-21 18:04:06 -0300

    Feature: Install wget on windows workflow

--- scripts/createWindowsRelease.sh
+++ scripts/createWindowsRelease.sh
@@ -7,6 +7,7 @@ OUTPUT_DIR="composeApp/build/compose/binaries/main-release/exe"
 OUTPUT="${OUTPUT_DIR}/${APP_NAME}-${APP_VERSION}.exe"
 
 echo "📥 Getting gradle..."
+choco install wget --no-progress
 wget -nc -q https://services.gradle.org/distributions/gradle-8.9-bin.zip -P ./gradle/wrapper/
 
 echo "🔨 Building release..."
