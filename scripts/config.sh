2293c64043cc21b37e5397838dd954fd0c50f342
Edney Osf <edney.osf@gmail.com>
2025-10-21 11:52:44 -0300

    Feat: Adding script to create Flatpak release (with config.sh)

---
+++ scripts/config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export ID="io.github.edneyosf.edconv"
+export APP_NAME="edconv"
+export APP_VERSION="1.4.1"
+export APP_DESCRIPTION_EN="An intuitive FFmpeg front-end for converting movies, shows and music"
+export APP_DESCRIPTION_PT="Uma interface intuitiva para FFmpeg para converter filmes, séries e músicas"
+export APP_AUTHOR="Edney Osf"
+export METADATA_DIR="./scripts/metadata/"
+export DESKTOP_FILE="${METADATA_DIR}${ID}.desktop"
+export APPDATA_FILE="${METADATA_DIR}${ID}.metainfo.xml"
