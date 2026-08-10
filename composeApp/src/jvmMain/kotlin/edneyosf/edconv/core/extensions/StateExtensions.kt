5eb753379dc951c0cc5a9154a15398c4d5449465
Edney Osf <edney.osf@gmail.com>
2026-08-10 16:37:55 -0300

    Remove Store and Add Mutation

--- composeApp/src/jvmMain/kotlin/edneyosf/edconv/core/extensions/StateExtensions.kt
+++ composeApp/src/jvmMain/kotlin/edneyosf/edconv/core/extensions/StateExtensions.kt
@@ -2,7 +2,18 @@ package edneyosf.edconv.core.extensions
 
 import androidx.compose.runtime.MutableState
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 
+interface Mutation<State> {
+    fun reduce(state: State): State
+}
+
+@Deprecated("Use reduce instead", ReplaceWith("value = value.block()"))
 inline fun <T> MutableState<T>.update(block: T.() -> T) { value = value.block() }
 
+@Deprecated("Use reduce instead", ReplaceWith("value = value.block()"))
 inline fun <T> MutableStateFlow<T>.update(block: T.() -> T) { value = value.block() }
+
+fun <State> MutableStateFlow<State>.mutate(mutation: Mutation<State>) {
+    update { currentState -> mutation.reduce(currentState) }
+}
